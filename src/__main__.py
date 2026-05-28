d9e3847305b808978a902ecf559f67612a6363be
Florent L <florent@linguenheld.net>
2026-05-28 23:09:31 +0200

    Add logic to get the next token

--- src/__main__.py
+++ src/__main__.py
@@ -1,3 +1,6 @@
+from typing import List, Any
+from dataclasses import dataclass
+from llm_sdk import Small_LLM_Model
 from models.prompt import parse_prompts
 from models.function_definition import (
     # FunctionDefinition,
@@ -6,6 +9,47 @@ from models.function_definition import (
 from parser_call_me import parse_call_me
 from termcolor import cprint
 from error.error import CallMeError
+
+
+@dataclass()
+class LLMManager:
+    prompt: str
+    llm: Small_LLM_Model = Small_LLM_Model()
+
+    def __post_init__(self) -> None:
+        self._open_brace_token = self._index_of("{")
+        self._close_brace_token = self._index_of("}")
+        self._quote = self._index_of('"')
+        self._colon = self._index_of(":")
+        self._answer_tokens: List[int] = []
+        self._prompt_tokens = self.llm.encode(self.prompt)[0].tolist()
+        self._prompt_tokens.append(self._open_brace_token)
+
+    def _index_of(self, who: str) -> int | Any:
+        """Get the index of who (works for one char)"""
+        return self.llm.encode(who)[0].tolist()[0]
+
+    def _index_max_value(self, values: List[float]) -> int:
+        """Get the index of the maximum value"""
+        index = 0
+        for i in range(1, len(values) - 1):
+            if values[i] > values[index]:
+                index = i
+
+        print(f"index found: {index} -> {len(values)}")
+        return index
+
+    def next_token(self) -> None:
+
+        logits: List[float] = self.llm.get_logits_from_input_ids(
+            self._prompt_tokens
+        )
+        # Get the index of the maximum
+        maxi = self._index_max_value(logits)
+
+        print(f"maxi found: {maxi} -> Decoded -> '{self.llm.decode([maxi])}'")
+        self._prompt_tokens.append(maxi)
+
 
 if __name__ == "__main__":
     arguments = parse_call_me()
@@ -16,10 +60,11 @@ if __name__ == "__main__":
     else:
         try:
             model = parse_functions(arguments["definitions"])
-            print(model)
             prompts = parse_prompts(arguments["input"])
-            print(prompts)
+
+            llm = LLMManager("What is the sum of 2 and 3?")
+            for _ in range(10):
+                llm.next_token()
+
         except CallMeError as e:
             e.print()
-
-    print("hello")
